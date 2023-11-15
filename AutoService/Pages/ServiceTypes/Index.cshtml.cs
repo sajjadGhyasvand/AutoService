ec7e5c8edce95b7328616af0035ca38efbc767cb
sajjadGh1995@hotmail.com <sajjadGh1995@hotmail.com>
2023-11-15 16:20:18 +0330

    add-service type page and create button

--- AutoService/Pages/ServiceTypes/Index.cshtml.cs
+++ AutoService/Pages/ServiceTypes/Index.cshtml.cs
@@ -1,12 +1,24 @@
+using AutoService.Data;
+using AutoService.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace AutoService.Pages.ServiceTypes
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+        public List<ServiceType> ServiceTypes { get; set; }     
+        public async Task<IActionResult> OnGet()
+        {
+            ServiceTypes = await _context.ServiceTypes.ToListAsync();
+            return Page();
         }
     }
 }

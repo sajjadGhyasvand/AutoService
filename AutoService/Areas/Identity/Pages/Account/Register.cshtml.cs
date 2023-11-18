6127e67293b1a508d7c07fbb373d1300cc7a1280
sajjadGh1995@hotmail.com <sajjadGh1995@hotmail.com>
2023-11-18 16:47:14 +0330

    Validation of Register Page

--- AutoService/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ AutoService/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -74,28 +74,28 @@ namespace AutoService.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [EmailAddress]
-            [Display(Name = "Email")]
+            [Required(ErrorMessage = "لطفا {0} را وارد کنید. ")]
+            [EmailAddress(ErrorMessage = "{0} وارد شده معتبر نمی باشد")]
+            [Display(Name = "ایمیل")]
             public string Email { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [Required(ErrorMessage = "لطفا {0} را وارد کنید. ")]
+            [StringLength(100, MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "کلمه عبور")]
             public string Password { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Required(ErrorMessage = "لطفا {0} را وارد کنید. ")]
+            [Display(Name = " تکرار کلمه عبور")]
+            [Compare("Password", ErrorMessage = "کلمه های عبور مغایرت دارند")]
             public string ConfirmPassword { get; set; }
         }
 

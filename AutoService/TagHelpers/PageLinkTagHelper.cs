f86fab3503525791bfd998c11a68897a82aa769a
sajjadGh1995@hotmail.com <sajjadGh1995@hotmail.com>
2023-11-26 15:59:27 +0330

    compelete Pagein

--- AutoService/TagHelpers/PageLinkTagHelper.cs
+++ AutoService/TagHelpers/PageLinkTagHelper.cs
@@ -1,4 +1,5 @@
 ﻿using AutoService.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
@@ -19,10 +20,29 @@ namespace AutoService.TagHelpers
         [ViewContext]
         [HtmlAttributeNotBound]
         public ViewContext  ViewContext { get; set; }
-        public PaginingInfo PaginingInfo { get; set; }
+        public PaginingInfo PageModel { get; set; }
         public string PageAction { get; set; }
         public string PageClass { get; set; }
         public string PageClassNormal { get; set; }
         public string PageClassSelected { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            TagBuilder result = new TagBuilder("div");
+
+            for(int i=1;i<= PageModel.TotalPage;i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                string url = PageModel.UrlParam.Replace(":", i.ToString());
+                tag.Attributes["href"] = url;
+                tag.AddCssClass(PageClass);
+                tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                tag.InnerHtml.Append(i.ToString());
+                result.InnerHtml.AppendHtml(tag);
+            }
+
+            output.Content.AppendHtml(result.InnerHtml);
+        }
     }
 }
